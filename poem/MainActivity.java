4551685a4e1744f1fba2a1d1279fa0cc03f3a806
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-11-06 18:01:31 +0900

    Update MainActivity.java

--- poem/MainActivity.java
+++ poem/MainActivity.java
@@ -7,17 +7,23 @@ import android.text.TextUtils;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView body01;
+    TextView body01,body02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
 
         body01 = (TextView) findViewById(R.id.body);
+        body02 = (TextView) findViewById(R.id.body2);
+
         body01.setSingleLine(true);
         body01.setEllipsize(TextUtils.TruncateAt.MARQUEE);
         body01.setSelected(true);
-        setContentView(R.layout.activity_main);
+
+        body02.setSingleLine(true);
+        body02.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        body02.setSelected(true);
+
     }
 }
