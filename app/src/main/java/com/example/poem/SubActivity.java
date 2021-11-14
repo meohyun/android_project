4f2a0af997c27e8d604a989dd0162e0f4915ed7e
meohyun24 <eogus1954@naver.com>
2021-11-14 23:06:11 +0900

    poem_list_app modified 1

--- app/src/main/java/com/example/poem/SubActivity.java
+++ app/src/main/java/com/example/poem/SubActivity.java
@@ -1,12 +1,33 @@
 package com.example.poem;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SubActivity extends Activity {
+    Button button02;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.poem_frost);
+
+        Button button02 = (Button)findViewById(R.id.back_button02);
+
+        button02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/poem/SubActivity2.java
+++ app/src/main/java/com/example/poem/SubActivity2.java
@@ -1,12 +1,28 @@
 package com.example.poem;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SubActivity2 extends Activity {
+    Button button01;
+    
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.poem_star);
+
+        Button button01 = (Button)findViewById(R.id.back_button01);
+
+        button01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
