7faf13611b35cbe4e1a9b90134c9225fb7148403
meohyun24 <eogus1954@naver.com>
2021-11-15 21:51:18 +0900

    poem_list_app modified 2

--- app/src/main/java/com/example/poem/SubActivity2.java
+++ app/src/main/java/com/example/poem/SubActivity2.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SubActivity2 extends Activity {
     Button button01;
@@ -25,4 +26,5 @@ public class SubActivity2 extends Activity {
             }
         });
     }
+
 }
